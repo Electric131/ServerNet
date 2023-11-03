93406a2fcd83d97bd958ebedd81825ce73ba7f3f
Electric131 <39623923+Electric131@users.noreply.github.com>
2023-11-02 23:24:26 -0500

    Added file transfer pages

--- webserver.js
+++ webserver.js
@@ -1,12 +1,113 @@
 const express = require('express');
+const fileUpload = require("express-fileupload");
+const sanitize = require("sanitize-filename");
 const { createServer } = require('http');
 const WebSocket = require('ws');
 const path = require('path');
+const fs = require('fs');
 const { RoomHandler, Host, Client } = require('./structures.js');
 
 const wait = ms => new Promise(res => setTimeout(res, ms));
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(fileUpload());
+
+function renderPage(path, vars = {}) {
+    return new Promise((res, rej) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) { console.log(err); return; };
+            let matches = data.matchAll(/\${{(\S+)}}/gm);
+            for (const match of matches) {
+                data = data.replace(`$\{{${match[1]}}\}`, vars[match[1]]);
+            }
+            res(data);
+        });
+    });
+};
+
+if (fs.existsSync("./public/file-transfer/downloaded-files")) {
+    fs.rm("./public/file-transfer/downloaded-files", { recursive: true }, e => { fs.mkdirSync("./public/file-transfer/downloaded-files"); });
+} else {
+    fs.mkdirSync("./public/file-transfer/downloaded-files");
+};
+
+var tempfiles = {};
+app.all("/file-transfer/uploadfile", (req, res) => {
+    let success = false;
+    if (req.method == "POST") {
+        if (req.files && req.files.filename) {
+            try {
+                let name = req.files.filename.name;
+                name = sanitize(name.replaceAll(" ", "-")); // Sanitize file name
+                let path = "./public/file-transfer/downloaded-files/" + name;
+                req.files.filename.mv(path).then(file => { });
+                tempfiles[name] = new Date().getTime() + 60000 * 0.1;
+                setTimeout(function (filepath, name) {
+                    if (fs.existsSync(filepath)) { // Precaution to ensure file does exist still
+                        fs.unlink(filepath, err => { });
+                    };
+                    delete tempfiles[name];
+                }, 60000 * 0.1, path, name);
+                success = true;
+                res.redirect("/file-transfer/upload/?state=success&filename=" + encodeURIComponent(name));
+            } catch {};
+        };
+        // res.download() for downloads after
+    };
+    if (!success) {
+        res.redirect("/file-transfer/upload/?state=fail");
+    };
+})
+app.get("/file-transfer/upload", (req, res) => {
+    if (req.query && req.query.state) {
+        if (req.query.state == "success" && req.query.filename) {
+            renderPage('./file-transfer/upload.html', { header: `File Uploaded Successfully!`, info: `Uploaded as "${req.query.filename}"\nView at: <a href="/file-transfer/uploads/${req.query.filename}">${req.query.filename}</a>` }).then(data => {
+                res.send(data);
+            });
+        } else {
+            renderPage('./file-transfer/upload.html', { header: `File Upload Failed.`, info: `` }).then(data => {
+                res.send(data);
+            });
+        }
+        return;
+    }
+    renderPage('./file-transfer/upload.html', { header: ``, info: `` }).then(data => {
+        res.send(data);
+    });
+});
+app.get("/file-transfer/downloaded-files/*", (req, res) => {
+    if (fs.existsSync('./public' + req.url)) {
+        res.sendFile(path.join(__dirname, '/public' + req.url));
+    } else {
+        res.redirect("/file-transfer/uploads");
+    };
+});
+app.get("/file-transfer/uploads", (req, res) => {
+    let fileList = ["None"];
+    if (Object.keys(tempfiles).length > 0) {
+        fileList = [];
+        let time = new Date().getTime();
+        for (const filename of Object.keys(tempfiles)) {
+            if (tempfiles[filename] <= time) continue;
+            fileList.push(`<a href="/file-transfer/uploads/${filename}">${filename}</a>`);
+        }
+    }
+    renderPage('./file-transfer/uploads.html', { fileList: fileList.join("<br>") }).then(data => {
+        res.send(data);
+    });
+    return;
+});
+app.get("/file-transfer/uploads/*", (req, res) => {
+    let file = req.url.replace(/^\/file-transfer\/uploads\//, "");
+    if (fs.existsSync("./public/file-transfer/downloaded-files/" + file)) {
+        renderPage('./file-transfer/view-file.html', { finalTime: tempfiles[file], filename: file }).then(data => {
+            res.send(data);
+        });
+    } else {
+        res.redirect("/file-transfer/uploads");
+    };
+    return;
+});
 
 var roomHandler = new RoomHandler();
 app.post("/newRoom/", (req, res) => {
